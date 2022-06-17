6ae71d9af8caa8b60a4715454dbabaef3786b1c8
danalee <eleeje97@gmail.com>
2022-06-17 16:53:59 +0900

    update urls.py

--- learningmate/urls.py
+++ learningmate/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('dailyclass/', include('dailyclass.urls')),
-    path('community/', include('community.urls'))
+    # path('', include('accounts.urls')),
+    # path('dailyclass/', include('dailyclass.urls')),
+    # path('community/', include('community.urls'))
 ]
