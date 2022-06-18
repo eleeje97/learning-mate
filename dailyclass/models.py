b2858d9397a0893ac537d5f27bee8b17e14d63d4
12345098 <o0324u@naver.com>
2022-06-18 18:52:32 +0900

    오류 수정했습니당

--- dailyclass/models.py
+++ dailyclass/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-# class Post(models.Model):
-#     #user_id =
-#     #file_url =
-#
-#     title = models.CharField(max_length=30)
-#     date = models.DateTimeField(auto_now_add=True)
-#     content = models.TextField()
-#
-#     def __str__(self):
-#         return f'[{self.pk}]{self.title}'
+class Post(models.Model):
+    #user_id =
+    #file_url =
 
-class Bookmark(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.URLField(verbose_name='Site URL')
+    title = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+# class Bookmark(models.Model):
+#     title = models.CharField(max_length=200)
+#     url = models.URLField(verbose_name='Site URL')
