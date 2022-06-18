524b643ca35b424b8760f2307adc3ebe895822f1
danalee <eleeje97@gmail.com>
2022-06-18 18:18:30 +0900

    login 페이지

--- learningmate/settings.py
+++ learningmate/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'community',
-    'dailyclass'
+    'dailyclass',
+    'common'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
