f2084a253f1da0b337db126607b8ce6d52eedb6e
danalee <eleeje97@gmail.com>
2022-06-21 04:55:21 +0900

    file download, delete

--- dailyclass/models.py
+++ dailyclass/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 from accounts.models import User
@@ -31,3 +33,7 @@ class ClassMaterial(models.Model):
     file_url = models.FileField('uploaded_file', upload_to='class_material/')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
     comment = models.CharField(max_length=500, null=True, blank=True)
+    # file_type = models.CharField(max_length=100)
+
+    def get_filename(self):
+        return os.path.basename(self.file_url.name)
--- dailyclass/views.py
+++ dailyclass/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render, redirect
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.http import FileResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
 
 from .forms import QuestionForm
 from .models import QnA, QnA_answer, ClassMaterial
 
 
+# 학습자료 공유
 def classmaterial(request):
-    return render(request, 'dailyclass/classmaterial.html')
-
-
-def question_form(request):
-    return render(request, 'dailyclass/question_form.html',)
-
-  
-def test_view(request):
-    return render(request, 'dailyclass/test.html',)
+    upload_file(request)
+    file_list, file_names = fileList(request)
+    return render(request, 'dailyclass/classmaterial.html', {'file_list': file_list, 'file_names': file_names})
 
 
 def upload_file(request):
@@ -24,16 +26,62 @@ def upload_file(request):
         print(request.user)
         print(request.POST['comment'])
         if request.FILES:
-            material = ClassMaterial(comment=request.POST['comment'], file_url=request.FILES['uploaded_file'], user_id=request.user)
+            # material = ClassMaterial(comment=request.POST['comment'], file_name=str(request.FILES['uploaded_file']), file_url=request.FILES['uploaded_file'], user_id=request.user)
+            material = ClassMaterial(comment=request.POST['comment'],
+                                     file_url=request.FILES['uploaded_file'], user_id=request.user)
             print('파일:', request.FILES['uploaded_file'])
             print(material)
+            print(request.FILES['uploaded_file'].content_type)
             material.save()
 
-    return render(request, 'dailyclass/classmaterial.html')
+
+def fileList(request):
+    file_list = ClassMaterial.objects.all()
+    file_names = []
+    for i in file_list:
+        file_names.append(i.get_filename())
+    return file_list, file_names
+
+
+class FileDownloadView(SingleObjectMixin, View):
+    queryset = ClassMaterial.objects.all()
+
+    def get(self, request, file_id):
+        object = self.get_object(file_id)
+
+        file_path = object.file_url.path
+        # file_type = object.content_type  # django file object에 content type 속성이 없어서 따로 저장한 필드
+        fs = FileSystemStorage(file_path)
+        response = FileResponse(fs.open(file_path, 'rb')) #content_type=file_type
+        response['Content-Disposition'] = f'attachment; filename={object.get_filename()}'
+
+        return response
+
+    def get_object(self, queryset=None):
+        object = get_object_or_404(ClassMaterial, id=self.kwargs['file_id'])
+        return object
+
+
+def delete_file(request, file_id):
+    file = get_object_or_404(ClassMaterial, pk=file_id)
+    file_url = os.path.join(settings.MEDIA_ROOT, str(file.file_url))
+    os.remove(file_url)
+    file.delete()
+    return redirect('dailyclass:classmaterial')
+
+
+
+# Quiz
+
+
+# 질문있어요!
+def question_form(request):
+    return render(request, 'dailyclass/question_form.html',)
 
   
 def test_view(request):
     return render(request, 'dailyclass/test.html',)
+
 
 def question_list(request):
     questions = QnA.objects.all().order_by('-pk')
@@ -44,11 +92,13 @@ def question_list(request):
                       'questions':questions,
                   })
 
+
 def single_question_page(request, qna_id):
     question = QnA.objects.get(qna_id=qna_id)
 
     return render(request,'dailyclass/question/single_question_page.html',
                   {'question':question,})
+
 
 def question_create(request):
     if request.method == 'POST':
