c88ac6a6f92521764ca2abcced027be0bb7bc0aa
danalee <eleeje97@gmail.com>
2022-06-20 20:03:39 +0900

    file upload

--- dailyclass/urls.py
+++ dailyclass/urls.py
@@ -5,8 +5,7 @@ app_name = 'dailyclass'
 
 urlpatterns=[
     path('', views.classmaterial),
-    path('classmaterial/', views.classmaterial),
+    path('classmaterial/', views.upload_file, name='upload_file'),
     path('question_form/', views.question_form),
-    path('upload_file/', views.upload_file, name='upload_file'),
     path('test/', views.test_view),
 ]
--- dailyclass/views.py
+++ dailyclass/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from dailyclass.models import ClassMaterial
+
 
 def classmaterial(request):
     return render(request, 'dailyclass/classmaterial.html')
@@ -7,12 +9,21 @@ def classmaterial(request):
 def question_form(request):
     return render(request, 'dailyclass/question_form.html',)
 
-def upload_file(request):
-    if request.method == "POST":
-        print(request.FILES)
-        if request.FILES:
-            print('파일:',request.FILES['uploaded_file'])
-    return render(request, 'dailyclass/classmaterial.html')
   
 def test_view(request):
     return render(request, 'dailyclass/test.html',)
+
+
+def upload_file(request):
+    if request.method == "POST":
+        print(request.POST)
+        print(request.FILES)
+        print(request.user)
+        print(request.POST['comment'])
+        if request.FILES:
+            material = ClassMaterial(comment=request.POST['comment'], file_url=request.FILES['uploaded_file'], user_id=request.user)
+            print('파일:', request.FILES['uploaded_file'])
+            print(material)
+            material.save()
+
+    return render(request, 'dailyclass/classmaterial.html')
