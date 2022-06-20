cfc168110b29eb5f7b2556cc4fcb810c21e47ced
yjh295 <yhwan6396@gmail.com>
2022-06-20 20:08:24 +0900

    dailyclass question list

--- dailyclass/urls.py
+++ dailyclass/urls.py
@@ -9,4 +9,7 @@ urlpatterns=[
     path('question_form/', views.question_form),
     path('upload_file/', views.upload_file, name='upload_file'),
     path('test/', views.test_view),
+    path('question_list/', views.question_list, name='question_list'),
+    path('question/<int:qna_id>/', views.single_question_page),
+    path('question/create/', views.question_create, name='question_create'),
 ]
