1e84430b50b581e4edc88a7a1e570234f7b0ff66
Toastzam <wkfthd1234@gmail.com>
2022-06-20 15:37:18 +0900

    호잇

--- community/views.py
+++ community/views.py
@@ -6,14 +6,54 @@ from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.db.models import Q
 
 
-def index(request):
+def notice(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw)  # 답변 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'notice/question_list.html', context)
+
+
+def information (request):
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw)  # 답변 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'infomation/question_list.html', context)
+
+
+def chat(request):
+    page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw)  # 답변 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'community/question_list.html', context)
 
 
@@ -21,6 +61,7 @@ def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'community/question_detail.html', context)
+
 
 @login_required(login_url='accounts:login')
 def answer_create(request, question_id):
@@ -39,14 +80,17 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'community/question_detail.html', context)
 
+
 @login_required(login_url='accounts:login')
 def question_create(request):
     if request.method == 'POST':
+        print(request.POST)
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.user = request.user
             question.create_date = timezone.now()
+            question.board_type = request.POST['board_type']
             question.save()
             return redirect('community:index')
     else:
@@ -102,6 +146,7 @@ def answer_modify(request, answer_id):
     context = {'answer': answer, 'form': form}
     return render(request, 'community/answer_form.html', context)
 
+
 @login_required(login_url='accounts:login')
 def answer_delete(request, answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
@@ -110,3 +155,4 @@ def answer_delete(request, answer_id):
     else:
         answer.delete()
     return redirect('community:detail', question_id=answer.question.id)
+
