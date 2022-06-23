a9f8dcc5eaeca8222c2c70531203adcb71961bf6
Toastzam <wkfthd1234@gmail.com>
2022-06-23 12:26:38 +0900

    답글 채팅형식

--- community/models.py
+++ community/models.py
@@ -31,3 +31,19 @@ class Answer(models.Model):
 #     description = RichTextUploadingField(blank=True,null=True)
 #     body = models.TextField()
 # #    username = models.CharField(max_length=50)
+
+# class QnA(models.Model):
+#     question_tags = [('R', 'R'),
+#                      ('Python', 'Python'),
+#                      ('HTML', 'HTML'),
+#                      ('CSS', 'CSS'),
+#                      ('JavaScript', 'JavaScript'),
+#                      ('Django', 'Django'),
+#                      ('Others', 'Others'),
+#                      ]
+#     qna_id = models.BigAutoField(primary_key=True)
+#     qna_question = RichTextUploadingField(blank=True, null=True)
+#     qna_question_tag = models.CharField(max_length=10, choices=question_tags)
+#     date = models.DateTimeField(auto_now_add=True)
+#     user_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
+#     # on_delete=models.CASCADE의 의미는 이 유저의 계정이 삭제될 경우 질문도 함께 삭제된다는 의미
