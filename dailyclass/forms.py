9fd7402ff84223509ea16f3481e7b2961e1b8be2
yjh295 <yhwan6396@gmail.com>
2022-06-23 09:50:08 +0900

    dailyclass q-list and q-answer almost completed

--- dailyclass/forms.py
+++ dailyclass/forms.py
@@ -10,7 +10,7 @@ class QuestionForm(forms.ModelForm):
         widgets = {
             'qna_question': forms.Textarea(attrs={'class': 'form-control', 'rows': 10, 'required': True}),
             'qna_question_tag': forms.Select(attrs={'class': 'form-control'}, choices=QnA.question_tags),
-            'user_id': forms.TextInput(attrs={'class': 'form-control', 'id': 'elder', 'type': 'hidden'})
+            'user_id': forms.TextInput(attrs={'class': 'form-control', 'id': 'elder', 'type': 'hidden'}),
         }
 
         labels = {
@@ -32,6 +32,21 @@ class EditForm(forms.ModelForm):
         labels = {
             'qna_question': '질문이 무엇인가요? ',
             'qna_question_tag': '어떤 언어에 대한 질문인가요? ',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = QnA_answer
+        fields = ('qna_answer', 'user_id')
+
+        widgets = {
+            'qna_answer': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+            'user_id': forms.TextInput(attrs={'class': 'form-control', 'id': 'elder', 'type': 'hidden'}),
+        }
+
+        labels = {
+            'qna_answer': '답변이 무엇인가요? ',
         }
 
 
