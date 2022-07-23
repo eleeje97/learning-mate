fa0484be0e12712426a79fabc3dd2884fd20b504
hayoungd <hylove7844@naver.com>
2022-07-23 17:51:04 +0900

    퀴즈 모델

--- dailyclass/admin.py
+++ dailyclass/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import QnA, QnA_answer, Quiz, result
+from .models import QnA, QnA_answer
 
 admin.site.register(QnA)
 admin.site.register(QnA_answer)
-admin.site.register(Quiz)
-admin.site.register(result)
+
 
 
--- dailyclass/views.py
+++ dailyclass/views.py
@@ -13,7 +13,7 @@ from django.views.generic.detail import SingleObjectMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-from .models import QnA, QnA_answer, ClassMaterial, Quiz
+from .models import QnA, QnA_answer, ClassMaterial
 from .forms import QuestionForm, EditForm, CommentForm
 #from .models import Question, Answer, User
 from django.http import HttpResponseNotAllowed
@@ -84,39 +84,39 @@ def delete_file(request, file_id):
     return redirect('dailyclass:classmaterial')
 
 
-#퀴즈
+# #퀴즈
+#
+# def quiz_home(request):
+#     return render(request, 'dailyclass/quiz/quiz_home.html')
+#
+# def quiz(request):
+#     quiz_id=request.GET['quiz_id']
+#     quiz = Quiz.objects.filter(pk=quiz_id)
+#     return render(request, 'dailyclass/quiz/quiz.html',{'quiz':quiz})
+#
+# # def result(request):
+# #     res = result.object.all()
+# #     return render(request, 'dailyclass/quiz/result.html',) #{"res":res})
+#
+# # def score(request):
+# #     num = 1
+# #     if request.POST:
+# #         num = int(request.POST['answer_num'])
+# #         if quiz.answer == num:  #여기서 인덱스를 어땋게 확인할까
+# #             checklst=[]
+# #             checklst += result.checking() #boolean?????
 
-def quiz_home(request):
-    return render(request, 'dailyclass/quiz/quiz_home.html')
-
-def quiz(request):
-    quiz_id=request.GET['quiz_id']
-    quiz = Quiz.objects.filter(pk=quiz_id)
-    return render(request, 'dailyclass/quiz/quiz.html',{'quiz':quiz})
-
-# def result(request):
-#     res = result.object.all()
-#     return render(request, 'dailyclass/quiz/result.html',) #{"res":res})
-
+# # 질문있어요!
+# def test_view(request):
+#     return render(request, 'dailyclass/test.html',)
+#
 # def score(request):
 #     num = 1
 #     if request.POST:
 #         num = int(request.POST['answer_num'])
 #         if quiz.answer == num:  #여기서 인덱스를 어땋게 확인할까
 #             checklst=[]
-#             checklst += result.checking() #boolean?????
-
-# 질문있어요!
-def test_view(request):
-    return render(request, 'dailyclass/test.html',)
-
-def score(request):
-    num = 1
-    if request.POST:
-        num = int(request.POST['answer_num'])
-        if quiz.answer == num:  #여기서 인덱스를 어땋게 확인할까
-            checklst=[]
-            #checklst += result.checking() #boolean?????
+#             #checklst += result.checking() #boolean?????
 
 
 class question_list(ListView):
